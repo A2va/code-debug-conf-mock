a899e70fa32db668d8cddf1d311b2dfb0147e65f
A2va <49582555+A2va@users.noreply.github.com>
2022-11-21 23:22:13 +0100

    VSCode extension debug configuration mock

---
+++ README.md
@@ -0,0 +1,3 @@
+## Debug configuration mock
+
+This is a mock for [microsoft/vscode#166862](https://github.com/microsoft/vscode/issues/166862)
---
+++ src/extension.ts
@@ -0,0 +1,44 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { WorkspaceFolder } from 'vscode';
+
+
+class XmakeConfigurationProvider implements vscode.DebugConfigurationProvider {
+	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: vscode.DebugConfiguration, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration> {
+        config.type = 'python'; 
+        config.name = 'Launch';
+        config.request = 'launch';
+        config.stopOnEntry = true;
+
+		return config;
+	}
+}
+
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	const provider = new XmakeConfigurationProvider();
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('xmake', provider));
+
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "xmake-test" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('xmake-test.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from xmake-test!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
