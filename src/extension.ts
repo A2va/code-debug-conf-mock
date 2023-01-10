e57af5a72263360d437e68276653d5271b9fea89
A2va <49582555+A2va@users.noreply.github.com>
2023-01-10 19:16:50 +0100

    Rename extension, remove DebugConfigurationProvider

--- src/extension.ts
+++ src/extension.ts
@@ -4,53 +4,9 @@ import * as vscode from 'vscode';
 import { WorkspaceFolder } from 'vscode';
 
 
-async function getPythonExtension(): Promise<any> {
-	const pyExtensionId = 'ms-python.python';
-	const pyExt = vscode.extensions.getExtension(pyExtensionId);
-
-	if (!pyExt) {
-		return undefined;
-	}
-	if (!pyExt.isActive) {
-		await pyExt.activate();
-	}
-
-	return pyExt;
-}
-
-class XmakeConfigurationProvider implements vscode.DebugConfigurationProvider {
-
-	// public provideDebugConfigurations(folder: WorkspaceFolder | undefined, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration[]> {
-	// 	return [{
-	// 		name: `XMake`,
-	// 		type: 'xmake',
-	// 		request: 'launch',
-	// 	}]
-	// }
-
-	public resolveDebugConfiguration(folder: WorkspaceFolder | undefined, debugConfig: vscode.DebugConfiguration, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration> {
-		// const pyExt = getPythonExtension();
-
-		debugConfig.type = 'python';
-		debugConfig.name = 'Launch';
-		debugConfig.request = 'launch';
-		debugConfig.stopOnEntry = true;
-
-		const config = {name: "Test Node", request:"launch", type: "python", program:debugConfig.program}
-
-		return config;
-	}
-}
-
-
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	const provider = new XmakeConfigurationProvider();
-	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('xmake', provider));
-
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "xmake-test" is now active!');
